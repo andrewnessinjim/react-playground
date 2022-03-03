9e6f1e66d7bdf39b5af30a73a5c3fe1b47a24913
Andrew Nessin <andrewnessinjim@yahoo.com>
2022-03-03 17:42:04 +0530

    Stylize list

--- src/AppsList.js
+++ src/AppsList.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-
+import "./AppList.css"
 export default function AppList() {
     return (
-        <ol>
-            <Link to="/colorCounter">
-                <li>Color Counter</li>
-            </Link>
+        <ol className="app-list">
+            <li>
+                <Link to="/colorCounter">Color Counter</Link>
+            </li>
         </ol>
     )
 }
